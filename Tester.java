b07f7972fe051fd8351574dfcbbc4059b21eb6b2
Justin Dylan <justin82049@gmail.com>
2020-11-22 23:11:04 -0500

    rengs test case for realNumbers

--- Tester.java
+++ Tester.java
@@ -1,207 +1,221 @@
 public class Tester {
-    public static int ERR = 0;
-    public static boolean DEBUG = true;
-    public static void main(String[] args) {
-        if (args.length > 0 && Boolean.parseBoolean(args[0]) == false) DEBUG = false;
-        String test = "";
 
-        test = "RealNumber.getValue()";
-        try {
-            double rand = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            check(test, num.getValue(), rand);
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+  public static void main(String[] args) {
+    boolean failure = false;
 
-        test = "RealNumber.toString()";
-        try {
-            double rand = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            check(test, num.toString(), ""+rand);
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+    failure = equalsTester() || failure;
+    failure = realNumberToRealNumberAddTester(100) || failure;
+    failure = realNumberToRealNumberMultiplyTester(100) || failure;
+    failure = realNumberToRealNumberDivideTester(100) || failure;
+    failure = realNumberToRealNumberSubtractTester(100) || failure;
 
-        test = "RealNumber.equals(RealNumber other)";
-        try {
-            RealNumber num = new RealNumber(3.5);
-            RealNumber other = new RealNumber(3.500000001);
+    System.out.println("\n ~~~ Overall Result ~~~");
+    if (failure) {
+      System.out.println("You overcooked the turkey.");
+    } else {
+      System.out.println("Happy Thanksgiving!");
+    }
+  }
 
-            check(test, num.equals(other), true);
+  private static void passMessage (int testCase) {
+    System.out.println("Test case " + testCase + " passed.");
+  }
 
-            other = new RealNumber(1.1);
-            check(test, num.equals(other), false);
+  private static void errorMessage(int testCase, String expected, String actual) {
+    System.out.println("WE HAVE FAILED AT TEST CASE " + testCase);
+    System.out.println("EXPECTED: " + expected);
+    System.out.println("ACTUAL: " + actual);
+  }
 
-            RationalNumber other1 = new RationalNumber(7, 2);
-            check(test, num.equals(other1), true);
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+  private static void methodMessage(String method, boolean failure) {
+    if (failure) {
+      System.out.println("\nAt least one test case failed for " + method);
+    } else {
+      System.out.println(method + " PASSED");
+    }
+  }
 
-        test = "RealNumber.add(RealNumber other)";
-        try {
-            double rand = Math.random() * 1000;
-            double rand1 = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            RealNumber other = new RealNumber(rand1);
-            check(test, (num.add(other)).toString(), rand+rand1+"");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+  private static void testerMessage(String method) {
+    System.out.println("\n ~~~ " + method + " TESTER ~~~");
+  }
 
-        test = "RealNumber.subtract(RealNumber other)";
-        try {
-            double rand = Math.random() * 1000;
-            double rand1 = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            RealNumber other = new RealNumber(rand1);
-            check(test, (num.subtract(other)).toString(), rand-rand1+"");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+  public static boolean equalsTester() {
+    testerMessage("equalsTester()");
+    boolean failure = false;
 
-        test = "RealNumber.multiply(RealNumber other)";
-        try {
-            double rand = Math.random() * 1000;
-            double rand1 = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            RealNumber other = new RealNumber(rand1);
-            check(test, (num.multiply(other)).toString(), rand*rand1+"");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
+    RealNumber a;
+    RealNumber b;
+    double[][] vals = {
+      {0, 0},
+      {0, 1E-6},
+      {1E-6, 0},
+      {Math.PI, Math.PI - 1E-9},
+      {Math.PI, Math.PI + 1E-4},
+      {-Math.PI, Math.PI},
+      {Math.PI, Math.PI + 1}
+    };
+    boolean[] expectedEvals = {
+      true,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false
+    };
 
-        test = "RealNumber.divide(RealNumber other)";
-        try {
-            double rand = Math.random() * 1000;
-            double rand1 = Math.random() * 1000;
-            RealNumber num = new RealNumber(rand);
-            RealNumber other = new RealNumber(rand1);
-            check(test, (num.divide(other)).toString(), rand/rand1+"");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.getNumerator()";
-        try {
-            RationalNumber num = new RationalNumber(3, 7);
-            check(test, num.getNumerator(), 3);
-        } catch (RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.getDenominator()";
-        try {
-            RationalNumber num = new RationalNumber(3, 7);
-            check(test, num.getDenominator(), 7);
-        } catch (RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.getValue()";
-        try {
-            RationalNumber num = new RationalNumber(3, 7);
-            check(test, num.getValue(), (double) 3 / 7);
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.reciprocal()";
-        try {
-            int rand = (int) Math.floor(Math.random() * 1000);
-            int rand1 = (int) Math.floor(Math.random() * 1000);
-            RationalNumber num = new RationalNumber(rand, rand1);
-            check(test, num.reciprocal().toString(), (new RationalNumber(rand1, rand)).toString());
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.equals(RationalNumber other)";
-        try {
-            int rand = (int) Math.floor(Math.random() * 1000);
-            int rand1 = (int) Math.floor(Math.random() * 1000);
-            int rand2 = (int) Math.floor(Math.random() * 1000);
-            int rand3 = (int) Math.floor(Math.random() * 1000);
-            RationalNumber num = new RationalNumber(rand, rand1);
-            RationalNumber other = new RationalNumber(rand*2, rand1*2);
-            RationalNumber other1 = new RationalNumber(rand2, rand3);
-            check(test, num.equals(other), true);
-            check(test, num.equals(other1), false);
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.toString()";
-        try {
-            RationalNumber num = new RationalNumber(43, 7);
-            RationalNumber num1 = new RationalNumber(54, 0);
-            check(test, num.toString(), "43/7");
-            check(test, num1.toString(), "0/1");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.reduce()";
-        try {
-            RationalNumber num = new RationalNumber(210, 45);
-            check(test, num.toString(), "14/3");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.multiply(RationalNumber other)";
-        try {
-            RationalNumber num = new RationalNumber(21, 65);
-            RationalNumber other = new RationalNumber(42, 5);
-            check(test, num.multiply(other).toString(), "882/325");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.divide(RationalNumber other)";
-        try {
-            RationalNumber num = new RationalNumber(21, 65);
-            RationalNumber other = new RationalNumber(42, 5);
-            check(test, num.divide(other).toString(), "1/26");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.add(RationalNumber other)";
-        try {
-            RationalNumber num = new RationalNumber(21, 65);
-            RationalNumber other = new RationalNumber(42, 5);
-            check(test, num.add(other).toString(), "567/65");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        test = "RationalNumber.subtract(RationalNumber other)";
-        try {
-            RationalNumber num = new RationalNumber(21, 65);
-            RationalNumber other = new RationalNumber(42, 5);
-            check(test, num.subtract(other).toString(), "-105/13");
-        } catch(RuntimeException e) {
-            except(test, e);
-        }
-
-        if (ERR == 0) System.out.println("All good!");
-        else if (ERR == 1) System.out.println("Uh oh... 1 error found.");
-        else System.out.println("Uh oh... " + ERR + " errors found.");
+    for (int test = 0; test < vals.length; test++) {
+      a = new RealNumber(vals[test][0]);
+      b = new RealNumber(vals[test][1]);
+      if (expectedEvals[test] == a.equals(b)) {
+        //System.out.println("a.equals(b)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("a.equals(b)");
+        errorMessage(test, Boolean.toString(expectedEvals[test]), Boolean.toString(b.equals(a)));
+      }
+      if (expectedEvals[test] == b.equals(a)) {
+        //System.out.println("b.equals(a)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("b.equals(a)");
+        errorMessage(test, Boolean.toString(expectedEvals[test]), Boolean.toString(b.equals(a)));
+      }
     }
 
-    public static void check(String test, Object actual, Object expected) {
-        if (!actual.equals(expected)) {
-            System.out.println("Failure on " + test + ": Expected \"" +
-                                expected + "\", got \"" + actual + "\".");
-            ERR++;
-        }
+    methodMessage("equals()", failure);
+    return failure;
+  }
+
+  public static boolean realNumberToRealNumberAddTester(int tests) {
+    testerMessage("realNumberToRealNumberAddTester()");
+    boolean failure = false;
+
+    for (int test = 0; test < tests; test++) {
+      double a = Math.random();
+      double b = Math.random();
+      RealNumber expected = new RealNumber(a + b);
+      RealNumber a1 = new RealNumber(a);
+      RealNumber b1 = new RealNumber(b);
+      if (a1.add(b1).equals(expected)) {
+        //System.out.println("a1.add(b1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("a1.add(b1)");
+        errorMessage(test, (a + b) + "", a1.add(b1).toString());
+      }
+      if (b1.add(a1).equals(expected)) {
+        //System.out.println("b1.add(a1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("b1.add(a1)");
+        errorMessage(test, (a + b) + "", b1.add(a1).toString());
+      }
     }
 
-    public static void except(String test, RuntimeException e) {
-        System.out.println("Failure on " + test + ": RuntimeException");
-        if (DEBUG) System.out.println(e.toString());
-        ERR++;
+    methodMessage("realNumberToRealNumberAddTester()", failure);
+    return false;
+  }
+
+  public static boolean realNumberToRealNumberMultiplyTester(int tests) {
+    testerMessage("realNumberToRealNumberMultiplyTester()");
+    boolean failure = false;
+
+    for (int test = 0; test < tests; test++) {
+      double a = Math.random();
+      double b = Math.random();
+      RealNumber expected = new RealNumber(a * b);
+      RealNumber a1 = new RealNumber(a);
+      RealNumber b1 = new RealNumber(b);
+      if (a1.multiply(b1).equals(expected)) {
+        //System.out.println("a1.multiply(b1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("a1.multiply(b1)");
+        errorMessage(test, (a * b) + "", a1.multiply(b1).toString());
+      }
+      if (b1.multiply(a1).equals(expected)) {
+        //System.out.println("b1.multiply(a1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("b1.multiply(a1)");
+        errorMessage(test, (a * b) + "", b1.multiply(a1).toString());
+      }
     }
+
+    methodMessage("realNumberToRealNumberMultiplyTester()", failure);
+    return false;
+  }
+
+  public static boolean realNumberToRealNumberDivideTester(int tests) {
+    testerMessage("realNumberToRealNumberDivideTester()");
+    boolean failure = false;
+
+    for (int test = 0; test < tests; test++) {
+      double a = Math.random();
+      double b = Math.random();
+      RealNumber expectedAOverB = new RealNumber(a / b);
+      RealNumber expectedBOverA = new RealNumber(b / a);
+      RealNumber a1 = new RealNumber(a);
+      RealNumber b1 = new RealNumber(b);
+      if (a1.divide(b1).equals(expectedAOverB)) {
+        //System.out.println("a1.divide(b1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("a1.divide(b1)");
+        errorMessage(test, (a / b) + "", a1.divide(b1).toString());
+      }
+      if (b1.divide(a1).equals(expectedBOverA)) {
+        //System.out.println("b1.divide(a1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("b1.divide(a1)");
+        errorMessage(test, (b / a) + "", b1.divide(a1).toString());
+      }
+    }
+
+    methodMessage("realNumberToRealNumberDivideTester()", failure);
+    return false;
+  }
+
+  public static boolean realNumberToRealNumberSubtractTester(int tests) {
+    testerMessage("realNumberToRealNumberSubtractTester()");
+    boolean failure = false;
+
+    for (int test = 0; test < tests; test++) {
+      double a = Math.random();
+      double b = Math.random();
+      RealNumber expectedAMinusB = new RealNumber(a - b);
+      RealNumber expectedBMinusA = new RealNumber(b - a);
+      RealNumber a1 = new RealNumber(a);
+      RealNumber b1 = new RealNumber(b);
+      if (a1.subtract(b1).equals(expectedAMinusB)) {
+        //System.out.println("a1.subtract(b1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("a1.subtract(b1)");
+        errorMessage(test, (a - b) + "", a1.subtract(b1).toString());
+      }
+      if (b1.subtract(a1).equals(expectedBMinusA)) {
+        //System.out.println("b1.subtract(a1)");
+        //passMessage(test);
+      } else {
+        failure = true;
+        System.out.println("b1.subtract(a1)");
+        errorMessage(test, (b - a) + "", b1.subtract(a1).toString());
+      }
+    }
+
+    methodMessage("realNumberToRealNumberSubtractTester()", failure);
+    return false;
+  }
 }
